3253d522e0cb4a702b4fb80e6cbc0a11347e4134
dzevdeth <dzevdethalilovic02@gmail.com>
2023-04-05 23:01:29 +0200

    js

--- JavaScript/7.cas/app.js
+++ JavaScript/7.cas/app.js
@@ -1,30 +1,90 @@
-//1. Ispisati neparne brojeve od 1 do 20 na dva nacina.
-//2. Ispisati sve brojeve od 50 do 100 koji su deljivi sa 5
-//3. Ispisati dvostruku vrednost prirpdnih brojeva 0d 6 do 14
-//4. Suma neparnih prirodnih brojeva od 10 do 20
+//Korisnik unosi 2 broja:
+//Na osnovu toga koje je broj manji, interakcija se vrsi od njega do veceg broja
+//Ispisuju se brojevi od manjeg ka vecem
 
-//1.
-for (let i = 1; i < 21; i++) {
-  if (i % 2 != 0) console.log(i);
+const broj1 = +prompt("Unesite prvi broj"); //plus pretvara u broj
+const broj2 = prompt("Unesite drugi broj");
+
+if (isNaN(broj1) || isNaN(broj2)) {
+  console.log("Morate uneti korektne brojeve");
 }
 
-for (let i = 1; i < 20; i += 2) {
-  console.log(i);
+if (broj1 < broj2) {
+  for (let i = broj1; i <= broj2; i++) console.log(i);
+} else if (broj2 < broj1) {
+  for (let i = broj2; i <= broj1; i++) console.log(i);
+} else if (broj1 === broj2) {
+  console.log("Uneli ste iste brojeve");
 }
 
 console.log("drugi zadatak");
 
 //2
-for (let broj = 50; broj <= 100; broj++) {
-  if (broj % 5 === 0) {
-    console.log(broj);
-  }
+//Preko switch naredbe
+console.log(new Date().getDay());
+// getDay()- daje vrednost 0-6
+// switch (new Date().getDay())
+// Na osnovu vrednosti koju nam daje izraz iz switch naredbe, kroz case_ove i eventualne default
+// "Dans je radni dan"
+// Ugodno provedite vikend
+
+switch (new Date().getDay()) {
+  //   case 0:
+  //     console.log("Ugodno provedite vikend");
+  //     break;
+
+  //   case 6:
+  //     console.log("Ugodno provedite vikend");
+  //     break;
+
+  //   case 1:
+  //     console.log("Danas je radni dan");
+  //     break;
+
+  //   case 2:
+  //     console.log("Danas je radni dan");
+  //     break;
+
+  //   case 3:
+  //     console.log("Danas je radni dan");
+  //     break;
+
+  //   case 4:
+  //     console.log("Danas je radni dan");
+  //     break;
+
+  //   case 5:
+  //     console.log("Danas je radni dan");
+  //     break;
+
+  case 0:
+  case 6:
+    console.log("Ugodno provedite vikend");
+    break;
+
+  default:
+    console.log("Danas je radni dan");
 }
 
-console.log("cetvrti zadatak");
-//4
-let zbir = 0;
-for (let i = 11; i < 20; i += 2) {
-  zbir += i;
+//3
+//Prebrojati i sabrati brojeve deljive sa 5 u intervalu od 1 do n.
+console.log("treci zadatak");
+
+let n = +prompt("Unesite broj:");
+let suma = 0;
+let brojBrojeva = 0;
+
+for (let i = 1; i < n; i++) {
+  if (i % 5 === 0) {
+    suma += i;
+    brojBrojeva += 1;
+  }
 }
-console.log(zbir);
+console.log(suma);
+console.log(brojBrojeva);
+
+//Nek se izvrsi interacija od broja 99 do -99
+
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
